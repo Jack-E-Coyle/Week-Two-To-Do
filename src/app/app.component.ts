f1607585f4923a355014618303f688a56b3d17c5
Jack Coyle <60261884+Jack-E-Coyle@users.noreply.github.com>
2021-02-07 23:37:22 +0000

    Adding Title and Array List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Week-Two-To-Do';
+  title = 'To-Do';
+  thingsToDo = [
+    'Learn Javascript',
+    'Learn Angular',
+    'Learn Redux'
+  ];
 }
